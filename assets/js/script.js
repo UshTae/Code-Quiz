0bba0efc10affaba1d833a83768950da6c988aa8
subwayaintfresh <79260476+subwayaintfresh@users.noreply.github.com>
2021-03-28 00:42:39 +1100

    finishing questions object

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,38 +6,54 @@
 ​
 ​questions = [
     {
-        title: "",
+        title: "What type of language is JavaScript",
         choices: [
-
+            "Assembly-Language",
+            "Object-Based",
+            "Object-Orientated",
+            "High-level",
         ],
-        answer:"",
+        answer:"Object-Based",
     },
     {
-        title:"",
+        title:" 'Function' and 'Var' are known as: ",
         choices: [
-
+            "Keywords",
+            "Data types",
+            "Declaration statements",
+            "Prototypes",
         ],
-        answer:"",
+        answer:"Declaration statements",
     },
     {
-        title:"",
+        title:"Which of the following variables takes precedence over the others if the names are the same?",
         choices: [
-
+            "Global variable",
+            "The local element",
+            "The two of the above",
+            "None of the above",
         ],
-        answer:"",
+        answer:"The local element",
     },
     {
-        title:"",
+        title:"What type of statements are IF statements?.",
         choices: [
-
+            "Declaration",
+            "Block",
+            "Loop",
+            "Conditional",
         ],
-        answer:"",
+        answer:"Conditional",
     },
     {
-        title:"",
+        title:"Functions must be _ to execute",
         choices: [
+            "Deleted",
+            "Re-defined",
+            "Called",
+            "logged",
 
         ],
-        answer:"",
+        answer:"Called",
     },
 ]
