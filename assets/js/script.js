7a59fe7eb8cbe883bb0048a807ed645a076c8622
subwayaintfresh <79260476+subwayaintfresh@users.noreply.github.com>
2021-03-28 00:00:57 +1100

    adding questions to javascript and finishing title styling

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+//------------------------------------
+//QUESTIONS
+//------------------------------------
+​
+// list of all questions, choices, and answers.
+​
+​questions = [
+    {
+        title: "",
+        choices: [
+
+        ],
+        answer:"",
+    },
+    {
+        title:"",
+        choices: [
+
+        ],
+        answer:"",
+    },
+    {
+        title:"",
+        choices: [
+
+        ],
+        answer:"",
+    },
+    {
+        title:"",
+        choices: [
+
+        ],
+        answer:"",
+    },
+    {
+        title:"",
+        choices: [
+
+        ],
+        answer:"",
+    },
+]
