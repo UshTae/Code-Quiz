a46ae324a561c8cb0aa2382e9b67f93d9b06c2c8
subwayaintfresh <79260476+subwayaintfresh@users.noreply.github.com>
2021-03-30 01:34:11 +1100

    removing 5th option

--- assets/js/script.js
+++ assets/js/script.js
@@ -158,13 +158,7 @@ function firstQuestion(){
         var button1D = questions[0].choices[3];
         btn1D.textContent= button1D;
         lists[0].appendChild(btn1D);
-
-        var btn1E = document.createElement('button');
-        var button1E = questions[0].choices[4];
-        btn1E.textContent= button1E;
-        lists[0].appendChild(btn1E);
-    
-
+        
     //conditional for if a choice chosen, start questionChange()
 }
 
