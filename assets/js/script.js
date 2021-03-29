ad0969535906f9b999032dc0e0ae67754d932620
subwayaintfresh <79260476+subwayaintfresh@users.noreply.github.com>
2021-03-29 22:21:53 +1100

    adding console log

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,29 @@
 // dom elements 
 var startButton = document.querySelector(".start-Button");
+var titleScreen = document.querySelector(".title-screen");
 
 
 //variables to keep track of quiz state
 
 // list of first set of questions, choices, and answers.
 // Others to be appended after
-​var questions = 
-    {
-        title: "What type of language is JavaScript",
-        choices: [
-            "Assembly-Language",
-            "Object-Based",
-            "Object-Orientated",
-            "High-level",
-        ],
-        answer:"Object-Based",
-    }
 
 // listener event to start game when start button clicked
-startButton.addEventListener("click", startGame);
+
 
 //submit score listener
 
 // function to dictate what happens when button pressed 
 function startGame() {
     //hide start screen
+    if (element.matches(titleScreen))
+    var state = element.getAttribute("data-state");
+    
+
+    if (state === "visible") {
+        element.dataser.state = "hidden";
+        console.log("title screen is hidden")
+    }
     //unhide questions section
     //start timer 
     //show starting time
